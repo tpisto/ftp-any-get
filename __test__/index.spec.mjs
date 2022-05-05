c81bb5471ad855b26ac14fc742600292540ae368
Tommi Pisto <tommi@pis.to>
2022-05-06 01:35:47 +0300

    Updated tests.

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,18 @@
-import test from 'ava'
+import test from "ava";
 
-import { sum } from '../index.js'
+import { getFile } from "../index.js";
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
-})
+test("fetch file using FTP", async (t) => {
+  let file = (await getFile("ftp://demo:password@test.rebex.net/readme.txt")).toString();
+  t.is(file.length, 405);
+});
+
+test("fetch file using FTPS", async (t) => {
+  let file = (await getFile("ftps://demo:password@test.rebex.net/readme.txt")).toString();
+  t.is(file.length, 405);
+});
+
+test("fetch file using SFTP", async (t) => {
+  let file = (await getFile("sftp://demo:password@test.rebex.net/readme.txt")).toString();
+  t.is(file.length, 405);
+});
--- test.mjs
+++
@@ -1,3 +0,0 @@
-import { getFile } from "./index.js";
-
-console.log("From native", (await getFile("sftp://demo:password@test.rebex.net/readme.txt")).toString());
